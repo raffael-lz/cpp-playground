d8c2b41c71b6097711803a31cfa33d8c67145525
raffael87 <raffael.lorenz@gmail.com>
2019-04-18 00:02:40 +0200

    functional factory

---
+++ patterns/factory/functional_factory.cc
@@ -0,0 +1,41 @@
+/* clang++ functional_factory.cc -o functional_factory -std=c++14 -Werror */
+
+// in functional factory, factories are represented as lambda functions i.e.
+
+#include <map>
+#include <functional>
+#include <memory>
+#include <string>
+
+struct Car
+{
+  virtual ~Car() = default;
+  virtual void Drive() const = 0;
+};
+
+struct HighClassCar : public Car
+{
+  void Drive() const override {}
+};
+
+struct MiddleClassCar : public Car
+{
+  void Drive() const override {}
+};
+
+int main()
+{
+  std::map<std::string, std::function<std::unique_ptr<Car>()>> functional_factories;
+
+  functional_factories["E200"] = []()
+                      {
+                        return std::make_unique<MiddleClassCar>();
+                      };
+
+  functional_factories["911Carrera"] = []()
+                      {
+                        return std::make_unique<HighClassCar>();
+                      };
+
+  auto E200 = functional_factories.at("E200");
+}
